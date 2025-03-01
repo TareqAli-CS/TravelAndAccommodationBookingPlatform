9102ecae5a9f1ff53d0fede1b1b38dda9845babc
Tareq Ali <tareqkmail71@gmail.com>
2025-03-01 03:17:39 +0200

    Add City entity

---
+++ TravelAndAccommodationBookingPlatform.Data/Entities/City.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelAndAccommodationBookingPlatform.Data.Entities
+{
+    public class City
+    {
+        public int CityId { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public string PostOffice { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? ModifiedAt { get; set; }
+        public string? ImageUrl { get; set; }
+    }
+}
