23ca3ff194508440e93ec027749d5f9fef174701
Tareq Ali <tareqkmail71@gmail.com>
2025-03-01 12:59:56 +0200

    (Add): Booking Repository

---
+++ TravelAndAccommodationBookingPlatform.Data/Repositories/BookingRepository.cs
@@ -0,0 +1,33 @@
+﻿using TravelAndAccommodationBookingPlatform.Data.Entities;
+using TravelAndAccommodationBookingPlatform.Data.Repositories.Interfaces;
+
+namespace TravelAndAccommodationBookingPlatform.Data.Repositories
+{
+    public class BookingRepository : IBookingRepository
+    {
+        private readonly TravelAndAccommodationDbContext _context;
+
+        public BookingRepository(TravelAndAccommodationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task AddAsync(Booking entity)
+        {
+            try
+            {
+                await _context.Bookings.AddAsync(entity);
+            }catch (Exception ex)
+            {
+                throw new Exception("An error occurred while adding the booking: " + ex.Message);
+            }
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("An error occurred while saving the booking: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ TravelAndAccommodationBookingPlatform.Data/Repositories/Interfaces/IBookingRepository.cs
@@ -0,0 +1,10 @@
+﻿using TravelAndAccommodationBookingPlatform.Data.Entities;
+
+namespace TravelAndAccommodationBookingPlatform.Data.Repositories.Interfaces
+{
+    public interface IBookingRepository
+    {
+        Task AddAsync(Booking entity);
+        Task<List<Booking>> GetBookingsForRoomAsync(int roomId);
+    }
+}
