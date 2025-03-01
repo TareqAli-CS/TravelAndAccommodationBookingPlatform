2ed5a9eec02acfa29f77d72f2588036dff4332e6
Tareq Ali <tareqkmail71@gmail.com>
2025-03-01 03:23:29 +0200

    (Add): HotelImage entity

---
+++ TravelAndAccommodationBookingPlatform.Data/Entities/HotelImage.cs
@@ -0,0 +1,10 @@
+﻿namespace TravelAndAccommodationBookingPlatform.Data.Entities
+{
+    public class HotelImage
+    {
+        public int ImageId { get; set; }
+        public int HotelId { get; set; }
+        public string ImageUrl { get; set; }
+        public Hotel Hotel { get; set; }
+    }
+}
