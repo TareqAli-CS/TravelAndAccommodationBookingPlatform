7b8f749ebf10405f52fad9df42a40e93690f4339
Tareq Ali <tareqkmail71@gmail.com>
2025-03-01 12:44:50 +0200

    (Fix): Review & RoomImage entities

--- TravelAndAccommodationBookingPlatform.Data/Entities/Review.cs
+++ TravelAndAccommodationBookingPlatform.Data/Entities/Review.cs
@@ -4,7 +4,7 @@
     {
         public int ReviewId { get; set; }
         public int HotelId { get; set; }
-        public string UserId { get; set; }
+        public int UserId { get; set; }
         public int Rating { get; set; }
         public string Comment { get; set; }
         public DateTime DateCreated { get; set; }
--- TravelAndAccommodationBookingPlatform.Data/Entities/RoomImage.cs
+++ TravelAndAccommodationBookingPlatform.Data/Entities/RoomImage.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TravelAndAccommodationBookingPlatform.Data.Entities
+﻿namespace TravelAndAccommodationBookingPlatform.Data.Entities
 {
     public class RoomImage
     {
+        public int ImageId { get; set; }
+        public int RoomId { get; set; }
+        public string ImageUrl { get; set; }
+        public Room Room { get; set; }
     }
 }
