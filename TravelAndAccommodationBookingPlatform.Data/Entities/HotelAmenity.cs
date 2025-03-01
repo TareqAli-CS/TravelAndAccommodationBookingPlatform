4c56d240c554867cd65eb05320ee3402e5d0d3c1
Tareq Ali <tareqkmail71@gmail.com>
2025-03-01 03:44:50 +0200

    (Fix) class name

--- TravelAndAccommodationBookingPlatform.Data/Entities/HotelAmenity.cs
+++ TravelAndAccommodationBookingPlatform.Data/Entities/HotelAmenity.cs
@@ -1,6 +1,10 @@
 ﻿namespace TravelAndAccommodationBookingPlatform.Data.Entities
 {
-    public class HotelAmenities
+    public class HotelAmenity
     {
+        public int HotelId { get; set; }
+        public Hotel Hotel { get; set; }
+        public int AmenityId { get; set; }
+        public Amenity Amenity { get; set; }
     }
 }
