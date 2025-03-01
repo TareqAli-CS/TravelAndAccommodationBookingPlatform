d2018025feb3186cf97110c884438ae55e33e0c5
Tareq Ali <tareqkmail71@gmail.com>
2025-03-01 05:21:57 +0200

    (Add): Booking entity

---
+++ TravelAndAccommodationBookingPlatform.Data/Entities/Booking.cs
@@ -0,0 +1,14 @@
+﻿namespace TravelAndAccommodationBookingPlatform.Data.Entities
+{
+    public class Booking
+    {
+        public int BookingId { get; set; }
+        public int UserId { get; set; }
+        public int RoomId { get; set; }
+        public DateTime CheckInDate { get; set; }
+        public DateTime CheckOutDate { get; set; }
+        public decimal TotalPrice { get; set; }
+        public User User { get; set; }
+        public Room Room { get; set; }
+    }
+}
