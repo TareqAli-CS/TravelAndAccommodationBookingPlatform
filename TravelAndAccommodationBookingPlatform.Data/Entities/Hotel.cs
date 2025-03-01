454f6585c2ae1b7e7dc3f6689e841bc9d623e4f9
Tareq Ali <tareqkmail71@gmail.com>
2025-03-01 03:20:06 +0200

    Add Hotel entity

---
+++ TravelAndAccommodationBookingPlatform.Data/Entities/Hotel.cs
@@ -0,0 +1,16 @@
+﻿namespace TravelAndAccommodationBookingPlatform.Data.Entities
+{
+    public class Hotel
+    {
+        public int HotelId { get; set; }
+        public string Name { get; set; }
+        public int StarRating { get; set; }
+        public string Owner { get; set; }
+        public string Location { get; set; }
+        public string Description { get; set; }
+        public int CityId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? ModifiedAt { get; set; }
+        public City City { get; set; }
+    }
+}
