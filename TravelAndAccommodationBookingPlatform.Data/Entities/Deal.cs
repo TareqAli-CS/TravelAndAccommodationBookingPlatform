dc5fcb8afaddbbc8c73b98dcffab0239bd9f5958
Tareq Ali <tareqkmail71@gmail.com>
2025-03-01 05:33:46 +0200

    (Add): Deal entity

---
+++ TravelAndAccommodationBookingPlatform.Data/Entities/Deal.cs
@@ -0,0 +1,12 @@
+﻿namespace TravelAndAccommodationBookingPlatform.Data.Entities
+{
+    public class Deal
+    {
+        public int DealId { get; set; }
+        public int RoomId { get; set; }
+        public Room Room { get; set; }
+        public decimal DealPrice { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
