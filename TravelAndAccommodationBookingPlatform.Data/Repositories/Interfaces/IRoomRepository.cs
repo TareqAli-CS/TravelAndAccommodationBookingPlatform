28f49d328f85cab169c5af479cf7e49f7a005d6d
Tareq Ali <tareqkmail71@gmail.com>
2025-03-08 00:54:21 +0200

    (Add): Room Repository

---
+++ TravelAndAccommodationBookingPlatform.Data/Repositories/Interfaces/IRoomRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelAndAccommodationBookingPlatform.Data.Entities;
+
+namespace TravelAndAccommodationBookingPlatform.Data.Repositories.Interfaces
+{
+    public interface IRoomRepository
+    {
+        Task<List<Room>> GetAllAsync();
+        Task<Room> GetByIdAsync(int id);
+        Task AddAsync(Room entity);
+        Task UpdateAsync(Room entity);
+        Task DeleteAsync(Room entity);
+        Task<Room> GetRoomWithDealsAsync(int id);
+    }
+}
---
+++ TravelAndAccommodationBookingPlatform.Data/Repositories/RoomRepository.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelAndAccommodationBookingPlatform.Data.Entities;
+using TravelAndAccommodationBookingPlatform.Data.Repositories.Interfaces;
+
+namespace TravelAndAccommodationBookingPlatform.Data.Repositories
+{
+    public class RoomRepository : IRoomRepository
+    {
+        private readonly TravelAndAccommodationDbContext _context;
+        public RoomRepository(TravelAndAccommodationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task AddAsync(Room entity)
+        {
+            try
+            {
+                await _context.Rooms.AddAsync(entity);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException($"Error in AddAsync: {ex.Message}", ex);
+            }
+        }
+
+        public async Task DeleteAsync(Room entity)
+        {
+            try
+            {
+                _context.Rooms.Remove(entity);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException($"Error in DeleteAsync: {ex.Message}", ex);
+            }
+        }
+
+        public async Task<List<Room>> GetAllAsync()
+        {
+            return await _context.Rooms
+                        .Include(room => room.RoomImages)
+                        .ToListAsync();
+        }
+
+        public async Task<Room> GetByIdAsync(int id)
+        {
+            try
+            {
+                var room = await _context.Rooms
+                    .Include(r => r.Deals)
+                    .FirstOrDefaultAsync(r => r.RoomId == id);
+
+                if (room != null)
+                {
+                    await _context.Entry(room)
+                        .Collection(r => r.RoomImages)
+                        .LoadAsync();
+
+                    var currentDate = DateTime.Now;
+                    room.Deals = room.Deals.Where(deal =>
+                        deal.StartDate <= currentDate && currentDate <= deal.EndDate)
+                        .ToList();
+                }
+
+                return room;
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException($"Error in GetByIdAsync: {ex.Message}", ex);
+            }
+        }
+
+        public async Task<Room> GetRoomWithDealsAsync(int id)
+        {
+            try
+            {
+                var room = await _context.Rooms
+                    .Include(r => r.Deals)
+                    .FirstOrDefaultAsync(r => r.RoomId == id);
+                return room;
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException($"Error in GetRoomWithDealsAsync: {ex.Message}", ex);
+            }
+        }
+
+        public async Task UpdateAsync(Room entity)
+        {
+            try
+            {
+                _context.Rooms.Update(entity);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException($"Error in UpdateAsync: {ex.Message}", ex);
+            }
+        }
+    }
+}
