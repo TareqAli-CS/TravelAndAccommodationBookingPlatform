596c6ea80187bb02aee24d43e28664d2d8942efc
Tareq Ali <tareqkmail71@gmail.com>
2025-03-08 00:48:23 +0200

    (Add): User Repository

---
+++ TravelAndAccommodationBookingPlatform.Data/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelAndAccommodationBookingPlatform.Data.Entities;
+
+namespace TravelAndAccommodationBookingPlatform.Data.Repositories.Interfaces
+{
+    public interface IUserRepository
+    {
+        User GetByUsername(string username);
+    }
+}
---
+++ TravelAndAccommodationBookingPlatform.Data/Repositories/UserRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelAndAccommodationBookingPlatform.Data.Entities;
+using TravelAndAccommodationBookingPlatform.Data.Repositories.Interfaces;
+
+namespace TravelAndAccommodationBookingPlatform.Data.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly TravelAndAccommodationDbContext _context;
+        public UserRepository(TravelAndAccommodationDbContext context)
+        {
+            _context = context;
+        }
+        public User GetByUsername(string username)
+        {
+            return _context.Users.FirstOrDefault(u => u.Username == username);
+        }
+    }
+}
