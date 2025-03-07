d8e4d348f1dadda9f4587f00b3a05f41215b28ab
Tareq Ali <tareqkmail71@gmail.com>
2025-03-07 12:06:53 +0200

    (Add): DealRepository

---
+++ TravelAndAccommodationBookingPlatform.Data/Repositories/DealRepository.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TravelAndAccommodationBookingPlatform.Data.Entities;
+using TravelAndAccommodationBookingPlatform.Data.Repositories.Interfaces;
+
+namespace TravelAndAccommodationBookingPlatform.Data.Repositories
+{
+    public class DealRepository : IDealRepository
+    {
+        public readonly TravelAndAccommodationDbContext _context;
+        public DealRepository(TravelAndAccommodationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task AddAsync(Deal entity)
+        {
+            try
+            {
+                await _context.Deals.AddAsync(entity);
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException($"Error in AddAsync: {ex.Message}", ex);
+            }
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("An error occurred while saving the deal: " + ex.Message);
+            }
+        }
+
+        public async Task<Deal> GetDealByRoomAndDateAsync(int roomId, DateTime startDate, DateTime endDate)
+        {
+            return await _context.Deals
+                .FirstOrDefaultAsync(d => d.RoomId == roomId &&
+                    d.StartDate <= endDate  &&
+                    d.EndDate >= startDate);
+        }
+    }
+}
