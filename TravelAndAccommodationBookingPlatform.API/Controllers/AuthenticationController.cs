0ee5c8ec110986551468ded0c7c6589432d480ba
Tareq Ali <tareqkmail71@gmail.com>
2025-03-08 12:14:14 +0200

    Update route in authentication controller

--- TravelAndAccommodationBookingPlatform.API/Controllers/AuthenticationController.cs
+++ TravelAndAccommodationBookingPlatform.API/Controllers/AuthenticationController.cs
@@ -12,6 +12,7 @@ using TravelAndAccommodationBookingPlatform.Data.Entities;
 
 namespace TravelAndAccommodationBookingPlatform.API.Controllers
 {
+    [Route("api/authentication")]
     [ApiController]
     public class AuthenticationController : ControllerBase
     {
