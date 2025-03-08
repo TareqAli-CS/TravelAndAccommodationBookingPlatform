71f76d85907b496bac65054ba62d9bbc366b1e80
Tareq Ali <tareqkmail71@gmail.com>
2025-03-08 09:57:59 +0200

    (Add): Authentication controller

---
+++ TravelAndAccommodationBookingPlatform.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using TravelAndAccommodationBookingPlatform.Application.Dtos;
+using TravelAndAccommodationBookingPlatform.Application.Services.Interfaces;
+using TravelAndAccommodationBookingPlatform.Data.Entities;
+
+namespace TravelAndAccommodationBookingPlatform.API.Controllers
+{
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        private readonly IConfiguration _configuration;
+
+        public AuthenticationController(IUserService userService, IConfiguration configuration)
+        {
+            _userService = userService;
+            _configuration = configuration;
+        }
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginRequestDto request)
+        {
+            if (_userService.ValidateUserCredentials(request.Username, request.Password))
+            {
+                var user = _userService.GetUserByUsername(request.Username);
+                var token = GenerateJwtToken(user);
+                return Ok(new { Token = token });
+            }
+            else
+            {
+                return Unauthorized(new { Message = "Invalid username or password." });
+            }
+        }
+
+        private string GenerateJwtToken(User user)
+        {
+            var key = Encoding.ASCII.GetBytes(_configuration["JwtSettings:Secret"]);
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                new Claim(ClaimTypes.Name, user.Username),
+                new Claim("UserId", user.UserId.ToString()),
+                new Claim(ClaimTypes.Role, user.Role.ToString()),
+            }),
+                Expires = DateTime.UtcNow.AddHours(Convert.ToDouble(_configuration["JwtSettings:ExpirationHours"])),
+                Issuer = _configuration["JwtSettings:Issuer"],
+                Audience = _configuration["JwtSettings:Audience"],
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+
+    }
+}
